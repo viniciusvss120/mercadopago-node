dec71f6a066fec618778291cb8392a032c5a40e4
Vinicius Silva Souza <92170937+viniciusvss120@users.noreply.github.com>
2022-10-05 10:01:58 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -62,7 +62,13 @@ app.post("/not", (req, res) => {
         MercadoPago.payment.search({
             qs: filtro
         }).then(data => {
-            console.log(data)
+            let pagamento = data.body.results[0]
+
+            if(pagamento != undefined){
+                console.log(pagamento)
+            }else{
+                console.log("Pagamento não existe!")
+            }
         }).catch(err => {
             console.log(err)
         })
